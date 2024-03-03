2869648aef2b2fc8e7730506117e05c80dd13f88
DEV-COM <kwp@tlen.pl>
2024-03-03 15:39:46 +0100

    first commit

---
+++ my-first-vue/src/components/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+    <header>
+        <h1><span>✅</span>To do</h1>
+        <p>Piszemy apkę w Vue 3</p>
+    </header>
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-first-vue/src/components/MyFirst.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        Mój komponent w <p>Vue.js</p>
+    </div>
+</template>
+<style scoped>
+div {
+    color: red;
+}
+p {
+    color: yellow;
+}
+</style>
