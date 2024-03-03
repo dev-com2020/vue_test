ade73e0b6a360d12a3f3d0d1110ba7b8c12303f1
DEV-COM <kwp@tlen.pl>
2024-03-03 16:46:15 +0100

    16:46 3.3

--- my-first-vue/src/components/TodoList.vue
+++ my-first-vue/src/components/TodoList.vue
@@ -9,14 +9,38 @@ type Item = {
     checked?: boolean;
 }
 
-const listItems: Item[] = [
+const initListItems = ():void => {
+    if (storageItems.value?.length === 0) {
+        const listItems =
+  [
     { title: 'Tworzę listę TODO', checked: true },
     { title: 'Tworzę listę TODO2', checked: true },
     { title: 'Tworzę listę TODO3', checked: false },
     { title: 'Tworzę listę TODO4', checked: false },
     { title: 'Tworzę listę TODO5', checked: true },
-    { title: 'Tworzę listę TODO6' },
+    { title: 'Tworzę listę TODO6' ,checked: true},
 ]
+setToStorage(listItems)
+storageItems.value = listItems
+    }
+}
+
+const findItemInList = (item: Item): Item | undefined => {
+    return storageItems.value.find(
+        (itemInList: Item) => itemInList.title === item.title
+    )
+}
+const toggleItemChecked = (item: Item): void => {
+    item.checked = !item.checked
+}
+
+const updateItem = (item: Item): void => {
+    const updateItem = findItemInList(item)
+    if (updateItem) {
+        toggleItemChecked(updateItem)
+        setToStorage(storageItems.value)
+    }
+}
 
 const storageItems: Ref<Item[]> = ref([])
 
@@ -24,13 +48,29 @@ const setToStorage = (items: Item[]): void => {
     localStorage.setItem('list-items', JSON.stringify(items))
 }
 
+const getFromStorage = (): Item[] | [] => {
+    const stored = localStorage.getItem('list-items')
+    if (stored) {
+        return JSON.parse(stored)
+    }
+    return []
+}
+
+const sortedList = computed(() => [
+    ...storageItems.value].sort((a,b) => (a.checked ? 1: 0) - (b.checked ? 1: 0))
+)
+
+onMounted(() => {
+    initListItems()
+    storageItems.value = getFromStorage()
+})
 
 </script>
 
 <template>
     <ul>
-        <li :key="key" v-for="(item, key) in listItems">
-            <ListItemVue :is-checked="item.checked">{{ item.title }}</ListItemVue>
+        <li :key="key" v-for="(item, key) in sortedList">
+            <ListItemVue :is-checked="item.checked" v-on:click.prevent="updateItem(item)">{{ item.title }}</ListItemVue>
         </li>
     </ul>
 </template>
